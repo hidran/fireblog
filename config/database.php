585c7e51ec9610a132aceede880cb7bb9a711a16
hidran@gmail.com <hidran@gmail.com>
2020-10-26 23:17:22 +0100

    Adds database config and PDO test

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+// PDO
+return [
+    'driver' => 'mysql', // può essere sqllite,mssql, oci
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => 'hidran',
+    'database' => 'freeblog',
+    //'dsn' =>'mysql:host=localhost;dbname=freeblog;charset=utf8',
+    'options' => [
+        [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ],
+        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    ]
+];
--- public/index.php
+++ public/index.php
@@ -3,6 +3,12 @@
 use App\Controllers\PostController;
 
 chdir(dirname(__DIR__));
+try {
+    $conn = new PDO('mysql:dbname=freeblog;host=localhost','root','hidran');
+
+}catch (\PDOException $e){
+    die($e->getMessage());
+}
 require_once __DIR__.'/../app/Controllers/PostController.php';
 $controller = new PostController();
 $controller->show(1);
